5998f3dd27ca48ee1fd90eb310ba506169b880b3
Fabricio1352 <100040158+Fabricio1352@users.noreply.github.com>
2024-05-09 00:07:06 -0700

    Initial commit

---
+++ ConexionBD.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+/**
+ *
+ * @author fabri
+ */
+public class ConexionBD {
+    MongoClient mongoClient;
+    MongoDatabase database;
+    
+    public ConexionBD(){
+        CodecRegistry pojoCodedRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .codecRegistry(pojoCodedRegistry)
+                .build();
+        mongoClient = MongoClient.create(settings);
+        database = mongoClient.getDatabase("");
+    }
+}
